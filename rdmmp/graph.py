f5312fb61879ca3fe28bacfd3b68f3228683343d
collardm <maxencecollard@aol.Com>
2019-04-03 15:08:42 +0200

    Update graph.py
    export plot to png with matplotlib
    
    code not tested

--- rdmmp/graph.py
+++ rdmmp/graph.py
@@ -6,6 +6,8 @@ Created on Fri Mar 22 10:28:00 2019
 """
 # %%
 import pandas as pd
+import numpy as np
+import time
 import rdmmp.helpgraph as helpgraph
 
 import plotly 
@@ -13,6 +15,14 @@ import plotly
 from plotly.offline import plot
 import plotly.graph_objs as go
 
+# To plot pretty figures
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+import os
 
 # %% Hello world plotly version
 trace = {'x':[1, 2], 'y':[1, 2]}
@@ -26,7 +36,6 @@ fig = go.Figure(data=data, layout=layout)
 plotly.offline.plot(fig, filename='graph/hello.html')  # auto_open=False
 
 # %% Basic plot : bar plot of salaries
-df = 
 
 salary_stats = helpgraph.salary_job_stats(df)
 
@@ -133,3 +142,48 @@ stacked1 = go.Bar(x=salary_stats.columns,
 stacked2 = go.Bar(
 
 )
+
+# %% go back to Matplotlib and save in graph folder .png
+
+# Where to save the figures
+IMAGES_PATH = "graph"
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    date = time.strftime("%d%m%Y")
+    path = os.path.join(date, IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+# %% 
+salary_stats = helpgraph.salary_job_stats(df)
+     
+def plot_stats_salary(salary_stats):
+    """
+    Plot bar chart for mean/std salary of 5 jobs.
+    The html code goes in the folder "/graph"
+
+    Parameter:
+        salary_stats : a dataframe get with salary_jo_stats function
+
+    Returns:
+        nothing : the .png fig is save in "/graph" folder
+        """
+    
+    fig, ax = plt.subplots()
+    
+    job = list(salary_stats.columns.values)
+    job_pos = np.arange(len(job))
+    salary_mean = salary_stats['mean']
+    salary_std = salary_stats['std']
+    
+    ax.barh(job_pos, salary_mean, xerr=salary_std, align='center')
+    
+    ax.set_yticks(job_pos)
+    ax.set_yticklabels(job)
+    ax.set_xlabel('Mean and standard deviation Salary for each Job')
+    
+    save_fig("stats_salary_job")
+    
+    return
